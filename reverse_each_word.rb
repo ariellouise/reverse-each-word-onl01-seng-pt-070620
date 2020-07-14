19eb62c8ebc30f3211a2f5e3a7325377089b8b4f
ariel murphy <arielladders@gmail.com>
2020-07-14 15:17:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,8 @@
 #sentence1 -"Hello there, and how are you?"
 #sentence2- "Hi again, just making sure it's reversed!"
 
-def reverse_each_word(sentence_1)
+def reverse_each_word(sentence1)
+  sentence1.split
   
   
 end
