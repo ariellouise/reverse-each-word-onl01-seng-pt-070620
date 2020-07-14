5bad09aec4bddde1ebc7929d4770d7589b422fd8
ariel murphy <arielladders@gmail.com>
2020-07-14 15:15:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-#write a method reverse_each_word that takes a string argument and returns the same sentece with each word reversed in place, fist by using 'each' and then same method using 'collect'.
+#write a method reverse_each_word that takes a string argument and returns the same sentence with each word reversed in place, fist by using 'each' and then same method using 'collect'.
 
 #.split to turn string into an Array
 
 #sentence1 -"Hello there, and how are you?"
-#sentence2- ""
+#sentence2- "Hi again, just making sure it's reversed!"
 
 def reverse_each_word
   
